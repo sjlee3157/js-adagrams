615c481ab3b7117008c33329b188503054a6e93b
sjlee3157 <sjlee3157@gmail.com>
2018-11-20 22:07:20 -0800

    added requirements

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,7 +1,28 @@
 const Adagrams = {
   drawLetters() {
-    // Implement this method for wave 1
+    // return an array of 10 strings
   },
+  usesAvailableLetters(input, lettersInHand) {
+    // input is a string - an input word
+    // lettersInHand is an array of drawn letters (10 strings)
+    // returns true or false
+  },
+  scoreWord(word) {
+    // word is a string of characters
+    // return an integer (points)
+  },
+  highestScoreFrom(words) {
+    // words is an array of strings
+    // return a single hash with data of winning word and score
+        // word: ${string of word}
+        // score: ${score of the word}
+  },
+  isInEnglishDict(input) {
+    // input is a string
+    // returns true or false
+
+    // https://github.com/dwyl/english-words
+  }
 };
 
 // Do not remove this line or your tests will break!
